8670f41b95bcc97f3437be631f53ff419abe5c99
RinkalPanchal <rinkal.panchal@dnkmail.in>
2026-02-14 12:22:50 +0530

    Add a Source file

---
+++ 11_Pointers/05_Pointer_Function/array_of_function_pointer.c
@@ -0,0 +1,51 @@
+// Array of function pointers with parameters
+
+#include <stdio.h>
+
+
+int add(int a, int b){
+    return a + b;
+}
+
+
+int sub(int a , int b){
+    return a - b;
+}
+
+
+int mul(int a, int b){
+    return a * b;
+}
+
+int div(int a, int b){
+    return a/b ;
+}
+
+
+int main(){
+
+    int x, y;
+    int choice;
+
+    printf("Enter the value of x:");
+    scanf("%d" , &x);
+
+    printf("Enter the value of y:");
+    scanf("%d" , &y);
+    
+
+
+    int (*a[4])(int , int) = {&add , &sub , &mul , &div};
+
+    printf("0.add\n1.sub\n2.mul\n3.div\n");
+    scanf("%d", &choice);
+
+    if(choice>=0 && choice<=3){
+        int result = a[choice](x,y);
+        printf("result = %d\n", result);
+    }
+    else{
+        printf("Invalid choice\n");
+    }
+    return 0;
+}
